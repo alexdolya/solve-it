19ed591c8dd45400eeb5fcc0e92c5323e96cb447
alexdolya <alexdolya90@gmail.com>
2026-07-03 10:43:29 +0400

    Intersection solved

---
+++ src/main/java/ru/dolya/solve/it/Intersection.java
@@ -0,0 +1,24 @@
+package ru.dolya.solve.it;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Intersection {
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums1) {
+            set.add(num);
+        }
+
+        Set<Integer> result = new HashSet<>();
+        for (int num : nums2) {
+            if (set.contains(num)) {
+                result.add(num);
+            }
+        }
+        return result.stream()
+                .mapToInt(Number::intValue)
+                .toArray();
+    }
+}
